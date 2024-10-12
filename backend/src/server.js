2b05c31c157585ea0d2f772f692ca62a27dc8cdd
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-10-12 19:21:24 +0700

    add package cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,19 @@ require('dotenv').config() // Load biến môi trường từ file .env
 const express = require('express')
 const morgan = require('morgan') // Logging requests
 const routes = require('@routes/_index') // Import các routes của ứng dụng
-
+const cors = require('cors') // Import CORS middleware
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors()) // Cho phép tất cả các domain truy cập API
+app.use(morgan('dev'))
 app.use(express.json()) // Xử lý dữ liệu JSON từ body request
+
 app.use('/', routes) // Sử dụng các routes trong thư mục routes
 
 app.get('/', (req, res) => {
   res.send('Welcome to the API!')
 })
-
-app.use(morgan('combined'))
 
 // Lắng nghe máy chủ
 app.listen(PORT, () => {
