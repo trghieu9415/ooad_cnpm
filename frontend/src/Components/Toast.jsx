6bde6d33afb3a5bc5bb95ff45498372271474648
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-10-25 08:33:18 +0700

    update Toast

--- frontend/src/Components/Toast.jsx
+++ frontend/src/Components/Toast.jsx
@@ -2,26 +2,26 @@ import { useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-const Toast = ({ message, status }) => {
+const Toast = ({ message, status, duration = 3000 }) => {
   useEffect(() => {
     switch (status) {
       case 'success':
-        toast.success(message)
+        toast.success(message, { autoClose: duration })
         break
       case 'error':
-        toast.error(message)
+        toast.error(message, { autoClose: duration })
         break
       case 'info':
-        toast.info(message)
+        toast.info(message, { autoClose: duration })
         break
       case 'warn':
-        toast.warn(message)
+        toast.warn(message, { autoClose: duration })
         break
       default:
-        toast(message)
+        toast(message, { autoClose: duration })
         break
     }
-  }, [message, status])
+  }, [message, status, duration])
 
   return (
     <div>
