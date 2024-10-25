80c393ec77130f11caad55063f3e534534a5de2c
VuPhong258 <trannguyenvuphong258@gmail.com>
2024-10-25 11:15:02 +0700

    update pagination detail

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -2,18 +2,22 @@ import Header from '../Components/Admin/Header/Header'
 import Sidebar from '../Components/Admin/Sidebar/Sidebar'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleDarkMode } from '../redux/slides/themeSlide'
+import { useState } from 'react'
 const AdminLayout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const dispatch = useDispatch()
   const darkMode = useSelector((state) => state.theme.darkMode)
-
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
   const handleToggleDarkMode = () => {
     dispatch(toggleDarkMode())
   }
   return (
     <>
       <div className={`${darkMode ? 'dark' : ''}`}>
-        <Header toggleDarkMode={handleToggleDarkMode} darkMode={darkMode} />
-        <Sidebar />
+        <Header toggleDarkMode={handleToggleDarkMode} darkMode={darkMode} toggleSidebar={toggleSidebar} />
+        <Sidebar isSidebarOpen={isSidebarOpen} />
         {children}
       </div>
     </>
