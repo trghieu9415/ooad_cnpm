10e543a81df2260266829419f1d86e4d2b45c409
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-11-03 14:32:17 +0700

    api tag user

--- frontend/src/apis/tag.api.js
+++ frontend/src/apis/tag.api.js
@@ -1,3 +1,9 @@
 import http from '../utils/http'
 
 export const AllTag = () => http.get('/tag')
+
+export const getTagById = (id) => http.get(`/tag/${id}`)
+
+export const createTag = (body) => http.post('/tag/create', body)
+
+export const editTag = (id) => http.put(`/tag/update/${id}`)
