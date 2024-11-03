c5fc32826522cda13bcdc55191a807dcb31226ed
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-11-03 14:25:51 +0700

    handle Tag Admin ( add, edit tag )

--- backend/src/routes/admin/index.js
+++ backend/src/routes/admin/index.js
@@ -4,13 +4,13 @@ const router = express.Router()
 const AccountRoutes = require('./AccountAdmin.route')
 const MemberRoutes = require('./MemberAdmin.route')
 const QuestionRoutes = require('./QuestionAdmin.route')
-// const TagRoutes = require('./TagAdmin.route')
 const CommentRoutes = require('./CommentAdmin.route')
+const TagRoutes = require('./TagAdmin.route')
 
 router.use('/account', AccountRoutes)
 router.use('/member', MemberRoutes)
 router.use('/question', QuestionRoutes)
-// router.use('/tag', TagRoutes)
 router.use('/comment', CommentRoutes)
+router.use('/tag', TagRoutes)
 
 module.exports = router
