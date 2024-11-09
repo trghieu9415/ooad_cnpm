cb60ab684c9b40ca7ca15b4d9a3d429ee5353cfe
dhuyhoang1406 <dhuyhoang181@gmail.com>
2024-11-09 17:08:02 +0700

    Admin call api detail question

--- backend/src/routes/admin/index.js
+++ backend/src/routes/admin/index.js
@@ -6,11 +6,13 @@ const MemberRoutes = require('./MemberAdmin.route')
 const QuestionRoutes = require('./QuestionAdmin.route')
 const CommentRoutes = require('./CommentAdmin.route')
 const TagRoutes = require('./TagAdmin.route')
+const AnswerRoutes = require('./AnswerAdmin.route')
 
 router.use('/account', AccountRoutes)
 router.use('/member', MemberRoutes)
 router.use('/question', QuestionRoutes)
 router.use('/comment', CommentRoutes)
 router.use('/tag', TagRoutes)
+router.use('/answer', AnswerRoutes)
 
 module.exports = router
